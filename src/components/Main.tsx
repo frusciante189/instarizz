c162016ff95035b8a308d9fcdc2120dd9895014c
frusciante189 <mcagdasbayirli@gmail.com>
2025-10-21 20:44:54 +0300

    main component fixes for scroll

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -9,7 +9,7 @@ interface MainProps {
 
 const Main = ({ onStartOnboarding }: MainProps) => {
   return (
-    <div className="flex flex-col gap-7 overflow-y-auto flex-1 pb-4">
+    <div className="flex flex-col gap-7 overflow-y-auto flex-1 pb-4 h-svh">
       <h1 className="font-poppins font-extrabold text-xl tracking-normal text-black pt-5">
         Everyone knows: <br /> Instagram is the{" "}
         <span className="font-poppins font-extrabold text-xl tracking-normal text-white">
@@ -20,19 +20,21 @@ const Main = ({ onStartOnboarding }: MainProps) => {
       <img
         src="/onboarding.png"
         alt="InstaRizz"
-        className="max-h-[361px] object-contain"
+        className="max-h-[280px] object-contain"
       />
       <p className="font-poppins font-medium text-base tracking-normal">
         Your Instagram says more than you think. Whether in dating or real life,
         it&apos;s your social resume. This tool helps you analyze and upgrade it
         for maximum attraction.
       </p>
-      <Button onClick={onStartOnboarding} className="mx-auto">
-        <span className="text-black font-extrabold text-2xl leading-none">
-          Analyze my Profile
-        </span>
-      </Button>
-      <ProfileCounter />
+      <div className="flex flex-col gap-7 mt-auto">
+        <Button onClick={onStartOnboarding} className="mx-auto">
+          <span className="text-black font-extrabold text-2xl leading-none">
+            Analyze my Profile
+          </span>
+        </Button>
+        <ProfileCounter />
+      </div>
     </div>
   );
 };
